33017d11717255fd9f209ded39933b6ea1aeb2ad
Tyler Bindon <martica@martica.org>
2013-01-17 20:07:58 -0700

    Fixed + and * to work with floats as well as ints
    Even mixed together!

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,25 +1,50 @@
-fn fold_list( args:~[Expression], function:fn(int, int) -> int ) -> Expression {
-    let mut acc:int = match args.head() {
-        Int(first) => first,
+fn fold_list( args:~[Expression], int_func:fn(int, int) -> int,
+              float_func:fn(f64, f64) -> f64 ) -> Expression {
+    let mut integral:bool = true;
+    let mut int_acc:int = 0;
+    let mut float_acc:f64 = 0.0;
+    match args.head() {
+        Int(first) => int_acc = first,
+        Float(first) => { float_acc = first as f64; integral = false }
         _ => fail
     };
+
     for args.tail().each() |&expr| {
         match expr {
-            Int(factor) => acc = function(acc, factor),
+            Int(arg) => {
+                if integral {
+                    int_acc = int_func(int_acc, arg)
+                } else {
+                    float_acc = float_func(float_acc, arg as f64)
+                }
+            }
+            Float(arg) => {
+                if integral {
+                    integral = false;
+                    float_acc = float_func(int_acc as f64, arg as f64)
+                } else {
+                    float_acc = float_func(float_acc, arg as f64)
+                }
+            }
             _ => fail
         }
     }
-    Int(acc)
+
+    if integral {
+        Int(int_acc)
+    } else {
+        Float(float_acc as float)
+    }
 }
 
 pub fn sum( addends:~[Expression],  _:@Environment) -> Expression {
-    fold_list( addends, int::add )
+    fold_list( addends, int::add, float::add )
 }
 
 pub fn multiply( factors:~[Expression], _:@Environment) -> Expression {
-    fold_list( factors, int::mul )
+    fold_list( factors, int::mul, float::mul )
 }
 
 pub fn subtract( args:~[Expression], _:@Environment) -> Expression {
-    fold_list( args, int::sub )
+    fold_list( args, int::sub, float::sub )
 }

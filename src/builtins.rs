85e2759b37faa48aab2a9bbac50d001215324b90
Tyler Bindon <martica@martica.org>
2013-01-19 13:36:20 -0700

    Have binary builtin functions work in unary case

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,16 +1,20 @@
 macro_rules! math_function {
-    ($function:ident) => {
+    ($function:ident $identity:expr) => {
         pub fn $function( args:~[Expression], _:@Environment) -> Expression {
-            vec::foldl(args.head(), args.tail(), |x, y| {x.$function(y)})
+            match args.len() {
+                0 => $identity,
+                1 => vec::foldl($identity, args, |x, y| {x.$function(y)}),
+                _ => vec::foldl(args.head(), args.tail(), |x, y| {x.$function(y)})
+            }
         }
     }
 }
 
-math_function!(add)
-math_function!(sub)
-math_function!(mul)
-math_function!(div)
-        
+math_function!(add Int(0))
+math_function!(sub Int(0))
+math_function!(mul Int(1))
+math_function!(div Int(1))
+
 pub fn equals( args:~[Expression], _:@Environment) -> Expression {
     for args.tail().each() |&expr| {
         if expr != args.head() {
